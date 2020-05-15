469311f2915350222a5eb56649a6293b881ee844
spenserdavies <62761290+spenserdavies@users.noreply.github.com>
2020-05-15 17:37:57 -0600

    upload main app files

--- app/main.js
+++ app/main.js
@@ -1,6 +1,5 @@
 import ListController from "./Controllers/ListController.js";
 
-//NOTE This should be good to go
 class App {
   listController = new ListController();
 }
--- app/store.js
+++ app/store.js
@@ -5,9 +5,6 @@ let _state = {
   lists: []
 };
 
-//NOTE You should not need to change the code from this point down
-
-//NOTE this method will get the lists from local storage at the start of the app
 function _loadState() {
   let data = JSON.parse(localStorage.getItem("TaskMaster"));
   if (data) {
@@ -18,18 +15,12 @@ function _loadState() {
 _loadState();
 
 class Store {
-  /**
-   * Provides access to application state data
-   */
   get State() {
     return _state;
   }
-
-  //NOTE call saveState everytime you change the state in any way
   saveState() {
     localStorage.setItem("TaskMaster", JSON.stringify(_state));
   }
 }
-
 const store = new Store();
 export default store;
