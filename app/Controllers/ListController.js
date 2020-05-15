fec0a43d943057585f09e2ad6cf104da27650238
spenserdavies <62761290+spenserdavies@users.noreply.github.com>
2020-05-15 17:38:24 -0600

    upload controller

--- app/Controllers/ListController.js
+++ app/Controllers/ListController.js
@@ -1,14 +1,50 @@
-import ListService from "../Services/ListService.js";
+import _listService from "../Services/ListService.js";
+import _store from "../store.js";
 
-//TODO Don't forget to render to the screen after every data change.
-function _drawLists() {}
-
-//Public
+function _drawLists() {
+  let lists = _store.State.lists
+  let template = ""
+  lists.forEach(l => template += l.Template)
+  document.getElementById("lists").innerHTML = template;
+}
 export default class ListController {
   constructor() {
-    //NOTE: After the store loads, we can automatically call to draw the lists.
     _drawLists();
   }
-
-  //TODO: Your app will need the ability to create, and delete both lists and listItems
+  addList(event){
+    event.preventDefault();
+    let rawList = {
+      name: event.target.name.value,
+      color: event.target.color.value,
+    }
+    console.log(rawList.color)
+    _listService.addList(rawList);
+    event.target.reset();
+    _drawLists();
+  }
+  addTask(event, listId){
+    event.preventDefault();
+    let task = event.target.task.value;
+    _listService.addTask(task, listId);
+    event.target.reset();
+    _drawLists();
+  }
+  removeList(listId){
+    let c = confirm("Delete This List?")
+    if(c == true){
+    _listService.removeList(listId);
+    } else {
+      return;
+    }
+    _drawLists();
+  }
+  removeTask(listId, index) {
+    let c = confirm("Delete this Task?")
+    if (c == true){
+    _listService.removeTask(listId, index);
+    } else {
+      return;
+    }
+    _drawLists();
+  }
 }
